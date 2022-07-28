6b0cd3a18c28e59399749ef3dc642dad27e7fb4b
hahyeong <comes.039@kurlycorp.com>
2022-07-28 13:39:19 +0900

    코드 정리

--- app/src/main/java/com/example/highcharttest/MainActivity.kt
+++ app/src/main/java/com/example/highcharttest/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.highsoft.highcharts.common.hichartsclasses.*
 import com.highsoft.highcharts.core.HIChartView
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
