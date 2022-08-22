1fd1f7e4ec1499610e0d4144c109779ca7995ecc
hahyeong <comes.039@kurlycorp.com>
2022-08-22 13:59:38 +0900

    툴팁 추가

---
+++ app/src/main/java/com/example/highcharttest/fragment/ReportOtherSymptomsNoSeizureFragment.kt
@@ -0,0 +1,39 @@
+package com.example.highcharttest.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.highcharttest.R
+import com.example.highcharttest.data.week
+import com.example.highcharttest.databinding.ReportOtherSymptomsNoSeizureBinding
+import com.example.highcharttest.databinding.ReportOtherSymptomsUnrecordedBinding
+
+class ReportOtherSymptomsNoSeizureFragment : Fragment() {
+
+    private lateinit var binding: ReportOtherSymptomsNoSeizureBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = ReportOtherSymptomsNoSeizureBinding.inflate(layoutInflater)
+
+
+        // 기본값 week 로설정
+        binding.date.text = getString(R.string.report_aura_date, week.startDate, week.endDate)
+        binding.add.setOnClickListener {
+
+//            val intent = Intent(getContext(), AllRecordActivity::class.java)
+//            startActivity(intent)
+
+        }
+
+        return binding.root
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/highcharttest/fragment/ReportOtherSymptomsUnrecordedFragment.kt
@@ -0,0 +1,38 @@
+package com.example.highcharttest.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.highcharttest.R
+import com.example.highcharttest.data.week
+import com.example.highcharttest.databinding.ReportOtherSymptomsUnrecordedBinding
+
+class ReportOtherSymptomsUnrecordedFragment : Fragment() {
+
+    private lateinit var binding: ReportOtherSymptomsUnrecordedBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = ReportOtherSymptomsUnrecordedBinding.inflate(layoutInflater)
+
+
+        // 기본값 week 로설정
+        binding.date.text = getString(R.string.report_aura_date, week.startDate, week.endDate)
+        binding.boxActive.setOnClickListener {
+
+//            val intent = Intent(getContext(), AllRecordActivity::class.java)
+//            startActivity(intent)
+
+        }
+
+        return binding.root
+
+    }
+
+
+}
